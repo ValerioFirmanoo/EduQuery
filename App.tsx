a18e570507afc70d7977d260bd8884a5565b782f
Federico Schermi <federicoschermi60@gmail.com>
2024-04-20 12:14:48 +0200

    start

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
+import * as React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+import Onboarding from './Tabs/Onboarding/Onboarding'
+
+const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
+
 export default function App() {
+  const [initialRouteName, setInitialRouteName] = React.useState<string>("Onboarding");
+
+  const routeName = 'Onboarding';
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator
+            screenOptions={{ headerShown: false }}
+            initialRouteName={initialRouteName}
+        >
+          <Stack.Screen name="Onboarding" component={Onboarding} />
+
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Tabs/Onboarding/Onboarding.tsx
@@ -0,0 +1,29 @@
+import React, {useState, useRef, useContext} from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    FlatList,
+    Animated,
+    TouchableOpacity,
+    useWindowDimensions,
+} from 'react-native'
+import {StatusBar} from "expo-status-bar";
+
+export default function OnboardingPage({ navigation }: any) {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
